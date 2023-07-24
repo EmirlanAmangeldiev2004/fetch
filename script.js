e55e772dcfb3fb845b6be32d885c69c6f004ee81
EmirlanAmangeldiev2004 <emirlanamangeldiev37@gmail.com>
2023-07-24 11:39:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// let API = "https://jsonplaceholder.typicode.com/users";
+
+// let list = document.querySelector(".list");
+// fetch(API)
+//   .then((data) => data.json())
+//   .then((unp) => {
+//     unp.forEach((el) => {
+//       list.innerHTML += `
+//          <div class = 'block'>
+//             <img class = 'image' src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&usqp=CAU'/>
+//             <div class = 'text'>
+//                <p class = 'name'>name: ${el.name}</p>
+//                <p class = 'userName'>username: ${el.username}</p>
+//                <p class = 'email'>email: ${el.email}</p>
+//                <p class = 'address'>address: ${el.address.city}</p>
+//                <p class = 'phone'>phone: ${el.phone}</p>
+//             </div>
+//          </div>
+//       `;
+//     });
+//   });
+
+//! online cinema
+
+let API = "https://www.omdbapi.com/?apikey=b6003d8a&s=All";
+
+let list = document.querySelector(".list");
+
+fetch(API)
+  .then((data) => data.json())
+  .then((unp) => {
+   unp.Search.forEach((el)=>{
+      console.log(el);
+      list.innerHTML += `
+         <div class = 'block'>
+            <img class = 'img' src = '${el.Poster}'>
+            <div class = 'text'>
+               <p class = 'name'>title: ${el.Title}</p>
+               <p class = 'year'>year: ${el.Year}</p>
+            </div>
+         </div>
+      `
+   })
+  });
